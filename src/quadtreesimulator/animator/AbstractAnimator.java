03f239e8985d0e2a4b8234f0fab9607884f7e92f
alexiossu <boringalexi@gmail.com>
2020-11-03 01:00:27 -0500

    Finished 1 part

--- src/quadtreesimulator/animator/AbstractAnimator.java
+++ src/quadtreesimulator/animator/AbstractAnimator.java
@@ -3,27 +3,54 @@ package quadtreesimulator.animator;
 
 import javafx.animation.AnimationTimer;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import quadtreesimulator.entity.FpsCounter;
 import quadtreesimulator.scene.AbstractScene;
 
 public abstract class AbstractAnimator extends AnimationTimer {
 
+	private boolean isRunning;
+	protected AbstractScene scene;
+	private FpsCounter fps;
+	
+	public AbstractAnimator() {
+		
+		fps = new FpsCounter(10, 25);
+		fps.getDrawable().setFill(Color.BLACK);
+		fps.getDrawable().setStroke(Color.WHITE);
+		fps.getDrawable().setWidth(1);
+	
+	}
+	@Override
 	public void stop() {
 		// TODO Auto-generated method stub
-		
+		super.stop();
+		if (isRunning) {
+			isRunning = false;
+		}
 	}
-
+	@Override
 	public void start() {
 		// TODO Auto-generated method stub
+		if (isRunning) {
+			isRunning = true;
+		}
+	}
+
+	public void clear() {
+		// TODO Auto-generated method stub
+		clearAndFill(scene.gc(), Color.TRANSPARENT);
+	}
+	
+	protected void clearAndFill(GraphicsContext gc, Color background) {
+		clearAndFill(gc, background, 0, 0, scene.w(), scene.h());
+	}
+	
+	protected void clearAndFill(GraphicsContext gc, Color background, double x, double y, double w, double h) {
 		
-	}
-
-	public Object clear() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void setScene(AbstractScene scene) {
-		// TODO Auto-generated method stub
+		gc.setFill(background);
+		gc.clearRect(x, y, w, h);
+		gc.fillRect(x, y, w, h);
 		
 	}
 	
